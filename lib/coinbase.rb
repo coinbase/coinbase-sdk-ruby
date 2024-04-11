20d2401946664ac01030dc85375d7f28fe9c6c33
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-11 14:00:01 -0400

    Add Coinbase class methods.

--- lib/coinbase.rb
+++ lib/coinbase.rb
@@ -2,3 +2,32 @@
 
 require_relative 'coinbase/network'
 require_relative 'coinbase/wallet'
+require 'json'
+
+# The Coinbase SDK.
+module Coinbase
+  # Initializes the Coinbase SDK using the passed API key JSON file.
+  # @param api_key_file [String] The path to the API key JSON file
+  def self.init_json(api_key_file)
+    file = File.read(api_key_file)
+    data = JSON.parse(file)
+    @api_key_name = data['name']
+    @api_key_secret = data['privateKey']
+  end
+
+  def self.api_key_name
+    @api_key_name
+  end
+
+  def self.api_key_name=(api_key_name)
+    @api_key_name = api_key_name
+  end
+
+  def self.api_key_secret
+    @api_key_secret
+  end
+
+  def self.api_key_secret=(api_key_secret)
+    @api_key_secret = api_key_secret
+  end
+end
