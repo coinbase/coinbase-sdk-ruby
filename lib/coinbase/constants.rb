af352f54e728f085691920b76b1c098082879417
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-16 11:28:13 -0400

    Add SUPPORTED_ASSETS constant

--- lib/coinbase/constants.rb
+++ lib/coinbase/constants.rb
@@ -18,4 +18,14 @@ module Coinbase
 
   # The amount of Wei per Gwei.
   WEI_PER_GWEI = 1_000_000_000
+
+  # The amount of Gwei per Ether.
+  GWEI_PER_ETHER = 1_000_000_000
+
+  # A map of supported Asset IDs.
+  SUPPORTED_ASSET_IDS = {
+    eth: true, # Ether, the native asset of most EVM networks.
+    gwei: true, # A medium denomination of Ether, typically used in gas prices.
+    wei: true # The smallest denomination of Ether.
+  }.freeze
 end
