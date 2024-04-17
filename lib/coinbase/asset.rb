81336ee465614bb3bbfabc51413e3333115c3b4f
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-17 11:08:44 -0400

    Create the Asset class

---
+++ lib/coinbase/asset.rb
@@ -0,0 +1,18 @@
+module Coinbase
+  # A representation of an Asset.
+  class Asset
+    attr_reader :network_id, :asset_id, :display_name, :address_id
+
+    # Returns a new Asset object.
+    # @param network_id [Symbol] The ID of the Network to which the Asset belongs
+    # @param asset_id [Symbol] The Asset ID
+    # @param display_name [String] The Asset's display name
+    # @param address_id [String] (Optional) The Asset's address ID, if one exists
+    def initialize(network_id, asset_id, display_name, address_id = nil)
+      @network_id = network_id
+      @asset_id = asset_id
+      @display_name = display_name
+      @address_id = address_id
+    end
+  end
+end
