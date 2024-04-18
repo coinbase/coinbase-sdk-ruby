b699037fa6c350ffb5d003071ae144bcaa1e254c
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-18 12:35:27 -0400

    Remove unused methods.

--- lib/coinbase.rb
+++ lib/coinbase.rb
@@ -6,7 +6,6 @@ require_relative 'coinbase/balance_map'
 require_relative 'coinbase/constants'
 require_relative 'coinbase/network'
 require_relative 'coinbase/transfer'
-require_relative 'coinbase/uniswap'
 require_relative 'coinbase/wallet'
 require 'dotenv'
 
@@ -15,21 +14,5 @@ module Coinbase
   # Initializes the Coinbase SDK.
   def self.init
     Dotenv.load
-  end
-
-  def self.api_key_name
-    @api_key_name
-  end
-
-  def self.api_key_name=(api_key_name)
-    @api_key_name = api_key_name
-  end
-
-  def self.api_key_secret
-    @api_key_secret
-  end
-
-  def self.api_key_secret=(api_key_secret)
-    @api_key_secret = api_key_secret
   end
 end
--- lib/coinbase/uniswap/constants.rb
+++
@@ -1,6 +0,0 @@
-module Coinbase
-  module Uniswap
-    # The Base Sepolia Router address.
-    BASE_SEPOLIA_ROUTER_ADDRESS = '0x050E797f3625EC8785265e1d9BDd4799b97528A1'
-  end
-end
