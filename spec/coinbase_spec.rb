a78e87952a46e17ebc55047823b650b5e315ab09
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-18 12:36:32 -0400

    Update coinbase_spec

--- spec/coinbase_spec.rb
+++ spec/coinbase_spec.rb
@@ -4,6 +4,7 @@ describe Coinbase do
   describe '#init' do
     it 'loads environment variables' do
       Coinbase.init
+      expect(ENV['BASE_SEPOLIA_RPC_URL']).to eq('https://sepolia.base.org')
     end
   end
 end
