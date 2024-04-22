c7c7d0057d51a1678efdfc62de81975e7682e274
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-22 14:02:48 -0400

    Update Coinbase object to take API key name and private key

--- lib/coinbase.rb
+++ lib/coinbase.rb
@@ -23,4 +23,32 @@ module Coinbase
   def self.base_sepolia_rpc_url=(value)
     @base_sepolia_rpc_url = value
   end
+
+  # Returns the API key name.
+  # @return [String] the API key name
+  def self.api_key_name
+    raise 'API key name is not set' unless @api_key_name
+
+    @api_key_name
+  end
+
+  # Sets the API key name.
+  # @param value [String] the API key name
+  def self.api_key_name=(value)
+    @api_key_name = value
+  end
+
+  # Returns the API key's private key.
+  # @return [String] the API key's private key
+  def self.api_key_private_key
+    raise 'API key private key is not set' unless @api_key_private_key
+
+    @api_key_private_key
+  end
+
+  # Sets the API key's private key.
+  # @param value [String] the API key's private key
+  def self.api_key_private_key=(value)
+    @api_key_private_key = value
+  end
 end
--- spec/coinbase_spec.rb
+++ spec/coinbase_spec.rb
@@ -13,4 +13,30 @@ describe Coinbase do
       expect(Coinbase.base_sepolia_rpc_url).to eq('https://not-sepolia.base.org')
     end
   end
+
+  describe '#api_key_name' do
+    it 'raises an error if the API key name is not set' do
+      expect { Coinbase.api_key_name }.to raise_error('API key name is not set')
+    end
+  end
+
+  describe '#api_key_name=' do
+    it 'sets the API key name' do
+      Coinbase.api_key_name = 'my-api-key-name'
+      expect(Coinbase.api_key_name).to eq('my-api-key-name')
+    end
+  end
+
+  describe '#api_key_private_key' do
+    it 'raises an error if the API key private key is not set' do
+      expect { Coinbase.api_key_private_key }.to raise_error('API key private key is not set')
+    end
+  end
+
+  describe '#api_key_private_key=' do
+    it 'sets the API key private key' do
+      Coinbase.api_key_private_key = 'my-api-key-private-key'
+      expect(Coinbase.api_key_private_key).to eq('my-api-key-private-key')
+    end
+  end
 end
