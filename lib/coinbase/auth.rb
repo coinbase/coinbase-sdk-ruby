3585cd91030d9a5e45499fc479a1adaaab51e44d
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-22 16:19:45 -0400

    Added authenticator

--- lib/coinbase/auth.rb
+++ lib/coinbase/auth.rb
@@ -5,8 +5,27 @@ require 'openssl'
 require 'securerandom'
 
 module Coinbase
-  # Methods for authenticating with the Coinbase Platform APIs.
-  module Auth
+  # A class that builds JWTs for authenticating with the Coinbase Platform APIs.
+  class Authenticator < Faraday::Middleware
+
+    # Initializes the Authenticator.
+    # @param app [Faraday::Connection] The Faraday connection
+    def initialize(app)
+      super(app)
+      @app = app
+    end
+
+    # Processes the request by adding the JWT to the Authorization header.
+    # @param env [Faraday::Env] The Faraday request environment
+    def call(env)
+      method = env.method.downcase.to_sym
+      uri = env.url.to_s
+      uri_without_protocol = URI(uri).host
+      build_jwt("#{method.upcase} #{uri_without_protocol}#{env.url.path}")
+      env.request_headers['Authorization'] = "Bearer #{token}"
+      @app.call(env)
+    end
+
     # Builds the JWT for the given API endpoint URI. The JWT is signed with the API key's private key.
     # @param uri [String] The API endpoint URI
     # @return [String] The JWT
