ff030ad5d45d252d5a457dcae8c2d5cc6d801b90
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-22 17:40:34 -0400

    Tests passing

--- spec/coinbase/authenticator_spec.rb
+++ spec/coinbase/authenticator_spec.rb
@@ -1,21 +1,35 @@
 # frozen_string_literal: true
 
 describe Coinbase::Authenticator do
-  let(:app) { double('Faraday::Connection' )}
+  let(:app) { double('Faraday::Connection') }
   let(:authenticator) { described_class.new(app) }
+  let(:data) { JSON.parse(File.read('spec/fixtures/cdp_api_key.json')) }
+  before do
+    Coinbase.init(data['name'], data['privateKey'])
+  end
 
   after(:each) do
     Coinbase.api_key_name = nil
     Coinbase.api_key_private_key = nil
   end
 
+  describe '#call' do
+    let(:env) { double('Faraday::Env') }
+
+    it 'adds the JWT to the Authorization header' do
+      allow(env).to receive(:method).and_return('GET')
+      allow(env).to receive(:url).and_return(URI('https://cdp.api.coinbase.com/v1/users/me'))
+      allow(env).to receive(:request_headers).and_return({})
+      expect(app).to receive(:call) do |env|
+        expect(env.request_headers['Authorization']).to start_with('Bearer ')
+      end
+
+      authenticator.call(env)
+    end
+  end
+
   describe '#build_jwt' do
     let(:uri) { 'https://cdp.api.coinbase.com/v1/users/me' }
-    let(:data) { JSON.parse(File.read('spec/fixtures/cdp_api_key.json')) }
-
-    before do
-      Coinbase.init(data['name'], data['privateKey'])
-    end
 
     it 'builds a JWT for the given endpoint URI' do
       jwt = authenticator.build_jwt(uri)
