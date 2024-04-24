389d4ef3c9579168ae3c0c5e91ef43af1ddb88ad
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-24 15:57:02 -0400

    All tests passing

--- spec/coinbase/wallet_spec.rb
+++ spec/coinbase/wallet_spec.rb
@@ -6,18 +6,25 @@ describe Coinbase::Wallet do
   let(:model) { Coinbase::Client::Wallet.new({ 'id': wallet_id, 'network_id': 'base-sepolia' }) }
   let(:wallets_api) { double('Coinbase::Client::WalletsApi') }
   let(:addresses_api) { double('Coinbase::Client::AddressesApi') }
-
-  subject(:wallet) { described_class.new(model, wallets_api, addresses_api, client: client) }
+  let(:address_model) {
+    Coinbase::Client::Address.new({
+      'address_id': '0xdeadbeef',
+      'wallet_id': wallet_id,
+      'public_key': '0x1234567890',
+      'network_id': 'base-sepolia'
+    })
+  }
 
   before do
-    allow(addresses_api).to receive(:create_address)
-    allow(addresses_api).to receive(:get_address)
+    allow(addresses_api).to receive(:create_address).and_return(address_model)
+    allow(addresses_api).to receive(:get_address).and_return(address_model)
+    @wallet = described_class.new(model, wallets_api, addresses_api, client: client)
   end
 
   describe '#initialize' do
     context 'when no seed or address count is provided' do
       it 'initializes a new Wallet' do
-        expect(wallet).to be_a(Coinbase::Wallet)
+        expect(@wallet).to be_a(Coinbase::Wallet)
       end
     end
 
@@ -49,28 +56,28 @@ describe Coinbase::Wallet do
 
   describe '#wallet_id' do
     it 'returns the Wallet ID' do
-      expect(wallet.wallet_id).to eq(wallet_id)
+      expect(@wallet.wallet_id).to eq(wallet_id)
     end
   end
 
   describe '#network_id' do
     it 'returns the Network ID' do
-      expect(wallet.network_id).to eq(:base_sepolia)
+      expect(@wallet.network_id).to eq(:base_sepolia)
     end
   end
 
   describe '#create_address' do
     it 'creates a new address' do
-      address = wallet.create_address
+      address = @wallet.create_address
       expect(address).to be_a(Coinbase::Address)
-      expect(wallet.list_addresses.length).to eq(2)
-      expect(address).not_to eq(wallet.default_address)
+      expect(@wallet.list_addresses.length).to eq(2)
+      expect(address).not_to eq(@wallet.default_address)
     end
   end
 
   describe '#default_address' do
     it 'returns the first address' do
-      expect(wallet.default_address).to eq(wallet.list_addresses.first)
+      expect(@wallet.default_address).to eq(@wallet.list_addresses.first)
     end
   end
 
@@ -82,42 +89,42 @@ describe Coinbase::Wallet do
     end
 
     it 'returns the correct address' do
-      default_address = wallet.default_address
-      expect(wallet.get_address(default_address.address_id)).to eq(default_address)
+      default_address = @wallet.default_address
+      expect(@wallet.get_address(default_address.address_id)).to eq(default_address)
     end
   end
 
   describe '#list_addresses' do
     it 'contains one address' do
-      expect(wallet.list_addresses.length).to eq(1)
+      expect(@wallet.list_addresses.length).to eq(1)
     end
   end
 
   describe '#list_balances' do
     before do
-      expect(wallet.default_address).to receive(:list_balances).and_return({ eth: BigDecimal(1) })
+      expect(@wallet.default_address).to receive(:list_balances).and_return({ eth: BigDecimal(1) })
     end
 
     it 'returns a hash with an ETH balance' do
-      expect(wallet.list_balances).to eq({ eth: BigDecimal(1) })
+      expect(@wallet.list_balances).to eq({ eth: BigDecimal(1) })
     end
   end
 
   describe '#get_balance' do
     before do
-      expect(wallet.default_address).to receive(:list_balances).and_return({ eth: BigDecimal(5) })
+      expect(@wallet.default_address).to receive(:list_balances).and_return({ eth: BigDecimal(5) })
     end
 
     it 'returns the correct ETH balance' do
-      expect(wallet.get_balance(:eth)).to eq(BigDecimal(5))
+      expect(@wallet.get_balance(:eth)).to eq(BigDecimal(5))
     end
 
     it 'returns the correct Gwei balance' do
-      expect(wallet.get_balance(:gwei)).to eq(BigDecimal(5 * Coinbase::GWEI_PER_ETHER))
+      expect(@wallet.get_balance(:gwei)).to eq(BigDecimal(5 * Coinbase::GWEI_PER_ETHER))
     end
 
     it 'returns the correct Wei balance' do
-      expect(wallet.get_balance(:wei)).to eq(BigDecimal(5 * Coinbase::WEI_PER_ETHER))
+      expect(@wallet.get_balance(:wei)).to eq(BigDecimal(5 * Coinbase::WEI_PER_ETHER))
     end
   end
 
@@ -131,24 +138,24 @@ describe Coinbase::Wallet do
       let(:to_address_id) { destination.default_address.address_id }
 
       before do
-        expect(wallet.default_address).to receive(:transfer).with(amount, asset_id, to_address_id).and_return(transfer)
+        expect(@wallet.default_address).to receive(:transfer).with(amount, asset_id, to_address_id).and_return(transfer)
       end
 
       it 'creates a transfer to the default address ID' do
-        expect(wallet.transfer(amount, asset_id, destination)).to eq(transfer)
+        expect(@wallet.transfer(amount, asset_id, destination)).to eq(transfer)
       end
     end
 
     context 'when the desination is an Address' do
-      let(:destination) { wallet.create_address }
+      let(:destination) { @wallet.create_address }
       let(:to_address_id) { destination.address_id }
 
       before do
-        expect(wallet.default_address).to receive(:transfer).with(amount, asset_id, to_address_id).and_return(transfer)
+        expect(@wallet.default_address).to receive(:transfer).with(amount, asset_id, to_address_id).and_return(transfer)
       end
 
       it 'creates a transfer to the address ID' do
-        expect(wallet.transfer(amount, asset_id, destination)).to eq(transfer)
+        expect(@wallet.transfer(amount, asset_id, destination)).to eq(transfer)
       end
     end
 
@@ -156,11 +163,11 @@ describe Coinbase::Wallet do
       let(:destination) { '0x1234567890' }
 
       before do
-        expect(wallet.default_address).to receive(:transfer).with(amount, asset_id, destination).and_return(transfer)
+        expect(@wallet.default_address).to receive(:transfer).with(amount, asset_id, destination).and_return(transfer)
       end
 
       it 'creates a transfer to the address ID' do
-        expect(wallet.transfer(amount, asset_id, destination)).to eq(transfer)
+        expect(@wallet.transfer(amount, asset_id, destination)).to eq(transfer)
       end
     end
   end
