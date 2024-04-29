ad0cccc670f08c08eba634bce3d59a3924e8f57f
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-29 12:49:18 -0400

    gsub

--- spec/e2e/production.rb
+++ spec/e2e/production.rb
@@ -4,8 +4,8 @@ Dotenv.load
 describe Coinbase do
   describe 'v0.0.2 SDK' do
     it 'behaves as expected' do
-      api_key_name = ENV['API_KEY_NAME'].gsub(' ', "\n")
-      api_key_private_key = ENV['API_KEY_PRIVATE_KEY'].gsub(' ', "\n")
+      api_key_name = ENV['API_KEY_NAME'].gsub('\n', "\n")
+      api_key_private_key = ENV['API_KEY_PRIVATE_KEY'].gsub('\n', "\n")
       Coinbase.init(api_key_name, api_key_private_key)
 
       puts "Fetching default user..."
