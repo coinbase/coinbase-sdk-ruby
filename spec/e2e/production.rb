140fc756f18847fac32173a82e378d712ba1f611
Yuga Cohler <yuga.cohler@coinbase.com>
2024-04-29 11:33:18 -0400

    Add log statement

--- spec/e2e/production.rb
+++ spec/e2e/production.rb
@@ -5,6 +5,8 @@ describe Coinbase do
   describe 'v0.0.2 SDK' do
     it 'behaves as expected' do
       Coinbase.init(ENV['API_KEY_NAME'], ENV['API_KEY_PRIVATE_KEY'])
+
+      puts "Fetching default user..."
       u = Coinbase.default_user
     end
   end
