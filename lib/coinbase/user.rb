7c9fe78962b3d2c537cfc60d0814825abe346029
yuga-cb <82042350+yuga-cb@users.noreply.github.com>
2024-05-24 08:17:25 -0400

    Change save_wallets_locally to save_seed! (#54)
    ### What changed? Why?
    - Change save_wallet_locally! to save_seed!
    - Change load_wallets to load_seed!
    - Return strings from Coinbase.configure methods
    - Add user.wallet method
    - Add page token to user.wallets method    
    
    #### Qualified Impact
    <!-- Please evaluate what components could be affected and what the
    impact would be if there was an
    error. How would this error be resolved, e.g. rollback a deploy, push a
    new fix, disable a feature
    flag, etc... -->

--- lib/coinbase/user.rb
+++ lib/coinbase/user.rb
@@ -40,7 +40,8 @@ module Coinbase
     # Lists the Wallets belonging to the User.
     # @param page_size [Integer] (Optional) the number of Wallets to return per page. Defaults to 10
     # @param next_page_token [String] (Optional) the token for the next page of Wallets
-    # @return [Coinbase::Wallet] the Wallets belonging to the User
+    # @return [Array<Coinbase::Wallet, String>] the Wallets belonging to the User and the pagination token, if
+    #   any.
     def wallets(page_size: 10, next_page_token: nil)
       opts = {
         limit: page_size
@@ -63,85 +64,26 @@ module Coinbase
         address_model_map[wallet_model.id] = addresses_list.data
       end
 
-      wallet_list.data.map do |wallet_model|
+      wallets = wallet_list.data.map do |wallet_model|
         Wallet.new(wallet_model, seed: '', address_models: address_model_map[wallet_model.id])
       end
+
+      [wallets, wallet_list.next_page]
     end
 
-    # Saves a wallet to local file system. Wallet saved this way can be re-instantiated with load_wallets_from_local
-    # function, provided the backup_file is available. This is an insecure method of storing wallet seeds and should
-    # only be used for development purposes. If you call save_wallet_locally! twice with wallets containing the same
-    # wallet_id, the backup will be overwritten during the second attempt.
-    # The default backup_file is `seeds.json` in the root folder. It can be configured by changing
-    # Coinbase.configuration.backup_file_path.
-    #
-    # @param wallet [Coinbase::Wallet] The wallet model to save.
-    # @param encrypt [bool] (Optional) Boolean representing whether the backup persisted to local file system should be
-    # encrypted or not. Data is unencrypted by default.
-    # @return [Coinbase::Wallet] the saved wallet.
-    def save_wallet_locally!(wallet, encrypt: false)
-      existing_seeds_in_store = existing_seeds
-      data = wallet.export
-      seed_to_store = data.seed
-      auth_tag = ''
-      iv = ''
-      if encrypt
-        shared_secret = store_encryption_key
-        cipher = OpenSSL::Cipher.new('aes-256-gcm').encrypt
-        cipher.key = OpenSSL::Digest.digest('SHA256', shared_secret)
-        iv = cipher.random_iv
-        cipher.iv = iv
-        cipher.auth_data = ''
-        encrypted_data = cipher.update(data.seed) + cipher.final
-        auth_tag = cipher.auth_tag.unpack1('H*')
-        iv = iv.unpack1('H*')
-        seed_to_store = encrypted_data.unpack1('H*')
+    # Returns the Wallet with the given ID.
+    # @param wallet_id [String] the ID of the Wallet
+    # @return [Coinbase::Wallet] the unhydrated Wallet
+    def wallet(wallet_id)
+      wallet_model = Coinbase.call_api do
+        wallets_api.get_wallet(wallet_id)
       end
 
-      existing_seeds_in_store[data.wallet_id] = {
-        seed: seed_to_store,
-        encrypted: encrypt,
-        auth_tag: auth_tag,
-        iv: iv
-      }
-
-      File.open(Coinbase.configuration.backup_file_path, 'w') do |file|
-        file.write(JSON.pretty_generate(existing_seeds_in_store))
+      addresses_list = Coinbase.call_api do
+        addresses_api.list_addresses(wallet_model.id, { limit: Coinbase::Wallet::MAX_ADDRESSES })
       end
-      wallet
-    end
 
-    # Loads all wallets belonging to the User with backup persisted to the local file system.
-    # @return [Map<String>Coinbase::Wallet] the map of wallet_ids to the wallets.
-    def load_wallets_from_local
-      existing_seeds_in_store = existing_seeds
-      raise ArgumentError, 'Backup file not found' if existing_seeds_in_store == {}
-
-      wallets = {}
-      existing_seeds_in_store.each do |wallet_id, seed_data|
-        seed = seed_data['seed']
-        raise ArgumentError, 'Malformed backup data' if seed.nil? || seed == ''
-
-        if seed_data['encrypted']
-          shared_secret = store_encryption_key
-          raise ArgumentError, 'Malformed encrypted seed data' if seed_data['iv'] == '' ||
-                                                                  seed_data['auth_tag'] == ''
-
-          cipher = OpenSSL::Cipher.new('aes-256-gcm').decrypt
-          cipher.key = OpenSSL::Digest.digest('SHA256', shared_secret)
-          iv = [seed_data['iv']].pack('H*')
-          cipher.iv = iv
-          auth_tag = [seed_data['auth_tag']].pack('H*')
-          cipher.auth_tag = auth_tag
-          cipher.auth_data = ''
-          hex_decoded_data = [seed_data['seed']].pack('H*')
-          seed = cipher.update(hex_decoded_data) + cipher.final
-        end
-
-        data = Coinbase::Wallet::Data.new(wallet_id: wallet_id, seed: seed)
-        wallets[wallet_id] = import_wallet(data)
-      end
-      wallets
+      Wallet.new(wallet_model, seed: '', address_models: addresses_list.data)
     end
 
     # Returns a string representation of the User.
@@ -164,23 +106,6 @@ module Coinbase
 
     def wallets_api
       @wallets_api ||= Coinbase::Client::WalletsApi.new(Coinbase.configuration.api_client)
-    end
-
-    def existing_seeds
-      existing_seed_data = '{}'
-      file_path = Coinbase.configuration.backup_file_path
-      existing_seed_data = File.read(file_path) if File.exist?(file_path)
-      output = JSON.parse(existing_seed_data)
-
-      raise ArgumentError, 'Malformed backup data' unless output.is_a?(Hash)
-
-      output
-    end
-
-    def store_encryption_key
-      pk = OpenSSL::PKey.read(Coinbase.configuration.api_key_private_key)
-      public_key = pk.public_key # use own public key to generate the shared secret.
-      pk.dh_compute_key(public_key)
     end
   end
 end
