81c8ce192f722fb2677a4d4d8b2fff439b9024b1
Alex Stone <alex.stone@coinbase.com>
2024-05-28 12:22:15 -0700

    [chore] Remove superfluous wallet transfer destination check
    This removes the check in wallet's transfer method that checks
    the destination type to validate the destination network and return
    the destination address, because this logic already occurs in
    the scope of the address transfer method.
    
    This now fully delegates the validations to the address transfer
    method.

--- lib/coinbase/wallet.rb
+++ lib/coinbase/wallet.rb
@@ -177,24 +177,14 @@ module Coinbase
       Coinbase::Balance.from_model_and_asset_id(response, asset_id).amount
     end
 
-    # Transfers the given amount of the given Asset to the given address. Only same-Network Transfers are supported.
-    # Currently only the default_address is used to source the Transfer.
+    # Transfers the given amount of the given Asset to the specified address or wallet.
+    # Only same-network Transfers are supported. Currently only the default_address is used to source the Transfer.
     # @param amount [Integer, Float, BigDecimal] The amount of the Asset to send
     # @param asset_id [Symbol] The ID of the Asset to send
     # @param destination [Wallet | Address | String] The destination of the transfer. If a Wallet, sends to the Wallet's
     #  default address. If a String, interprets it as the address ID.
     # @return [Transfer] The hash of the Transfer transaction.
     def transfer(amount, asset_id, destination)
-      if destination.is_a?(Wallet)
-        raise ArgumentError, 'Transfer must be on the same Network' if destination.network_id != network_id
-
-        destination = destination.default_address.id
-      elsif destination.is_a?(Address)
-        raise ArgumentError, 'Transfer must be on the same Network' if destination.network_id != network_id
-
-        destination = destination.id
-      end
-
       default_address.transfer(amount, asset_id, destination)
     end
 
