c0f89700792943af8653e36dd5c21321face20b8
Alex Stone <alex.stone@coinbase.com>
2024-05-28 17:01:37 -0700

    Add transaction and trade types
    This adds a Coinbase::Transaction and Coinbase::Trade type that
    will be used to interact with trade and transaction backend resources.

---
+++ lib/coinbase/transaction.rb
@@ -0,0 +1,122 @@
+# frozen_string_literal: true
+
+require_relative 'constants'
+require 'bigdecimal'
+require 'eth'
+require 'json'
+
+module Coinbase
+  # A representation of an onchain Transaction.
+  # Transactions should be constructed via higher level abstractions like Trade or Transfer.
+  class Transaction
+    # A representation of a Transaction status.
+    module Status
+      # The Transaction is awaiting being broadcast to the Network.
+      # At this point, transaction hashes may not yet be assigned.
+      PENDING = 'pending'
+
+      # The Transaction has been broadcast to the Network.
+      # At this point, at least the transaction hash should be assigned.
+      BROADCAST = 'broadcast'
+
+      # The Transaction is complete and has confirmed on the Network.
+      COMPLETE = 'complete'
+
+      # The Transaction has failed for some reason.
+      FAILED = 'failed'
+
+      # The states that are considered terminal on-chain.
+      TERMINAL_STATES = [COMPLETE, FAILED].freeze
+    end
+
+    # Returns a new Transaction object. Do not use this method directly.
+    # @param model [Coinbase::Client::Transaction] The underlying Transaction object
+    def initialize(model, from_address_id:)
+      raise unless model.is_a?(Coinbase::Client::Transaction)
+
+      @model = model
+      @from_address_id = from_address_id
+    end
+
+    attr_reader :from_address_id
+
+    # Returns the Unsigned Payload of the Transaction.
+    # @return [String] The Unsigned Payload
+    def unsigned_payload
+      @model.unsigned_payload
+    end
+
+    # Returns the Signed Payload of the Transaction.
+    # @return [String] The Signed Payload
+    def signed_payload
+      @model.signed_payload
+    end
+
+    # Returns the Transaction Hash of the Transaction.
+    # @return [String] The Transaction Hash
+    def transaction_hash
+      @model.transaction_hash
+    end
+
+    # Returns the status of the Transaction.
+    # @return [Symbol] The status
+    def status
+      @model.status
+    end
+
+    # Returns whether the Transaction is in a terminal state.
+    # @return [Boolean] Whether the Transaction is in a terminal state
+    def terminal_state?
+      Status::TERMINAL_STATES.include?(status)
+    end
+
+    # Returns the link to the transaction on the blockchain explorer.
+    # @return [String] The link to the transaction on the blockchain explorer
+    def transaction_link
+      # TODO: Parameterize this by Network.
+      "https://sepolia.basescan.org/tx/#{transaction_hash}"
+    end
+
+    # Returns the underlying raw transaction.
+    # @return [Eth::Tx::Eip1559] The raw transaction
+    def raw
+      return @raw unless @raw.nil?
+
+      raw_payload = [unsigned_payload].pack('H*')
+      parsed_payload = JSON.parse(raw_payload)
+
+      params = {
+        chain_id: parsed_payload['chainId'].to_i(16),
+        nonce: parsed_payload['nonce'].to_i(16),
+        priority_fee: parsed_payload['maxPriorityFeePerGas'].to_i(16),
+        max_gas_fee: parsed_payload['maxFeePerGas'].to_i(16),
+        gas_limit: parsed_payload['gas'].to_i(16), # TODO: Handle multiple currencies.
+        from: Eth::Address.new(from_address_id),
+        to: Eth::Address.new(parsed_payload['to']),
+        value: parsed_payload['value'].to_i(16),
+        data: parsed_payload['input'] || ''
+      }
+
+      @raw = Eth::Tx::Eip1559.new(Eth::Tx.validate_eip1559_params(params))
+    end
+
+    # Signs the Transaction with the provided key and returns the hex signing payload.
+    def sign(key)
+      raw.sign(key)
+
+      raw.hex
+    end
+
+    # Returns a String representation of the Transaction.
+    # @return [String] a String representation of the Transaction
+    def to_s
+      "Coinbase::Transaction{transaction_hash: '#{transaction_hash}', status: '#{status}'}"
+    end
+
+    # Same as to_s.
+    # @return [String] a String representation of the Transaction
+    def inspect
+      to_s
+    end
+  end
+end
---
+++ spec/unit/coinbase/transaction_spec.rb
@@ -0,0 +1,188 @@
+# frozen_string_literal: true
+
+describe Coinbase::Transaction do
+  let(:from_key) { Eth::Key.new }
+  let(:network_id) { :base_sepolia }
+  let(:wallet_id) { SecureRandom.uuid }
+  let(:from_address_id) { from_key.address.to_s }
+  let(:amount) { BigDecimal(100) }
+  let(:eth_amount) { amount / BigDecimal(Coinbase::WEI_PER_ETHER.to_s) }
+  let(:to_address_id) { '0x4D9E4F3f4D1A8B5F4f7b1F5b5C7b8d6b2B3b1b0b' }
+  let(:unsigned_payload) do \
+    '7b2274797065223a22307832222c22636861696e4964223a2230783134613334222c226e6f6e63' \
+'65223a22307830222c22746f223a22307834643965346633663464316138623566346637623166' \
+'356235633762386436623262336231623062222c22676173223a22307835323038222c22676173' \
+'5072696365223a6e756c6c2c226d61785072696f72697479466565506572476173223a223078' \
+'3539363832663030222c226d6178466565506572476173223a2230783539363832663030222c22' \
+'76616c7565223a2230783536626337356532643633313030303030222c22696e707574223a22' \
+'3078222c226163636573734c697374223a5b5d2c2276223a22307830222c2272223a2230783022' \
+'2c2273223a22307830222c2279506172697479223a22307830222c2268617368223a2230783664' \
+'633334306534643663323633653363396561396135656438646561346332383966613861363966' \
+'3031653635393462333732386230386138323335333433227d'
+  end
+  let(:signed_payload) do \
+    '02f86b83014a3401830f4240830f4350825208946cd01c0f55ce9e0bf78f5e90f72b4345b' \
+    '16d515d0280c001a0566afb8ab09129b3f5b666c3a1e4a7e92ae12bbee8c75b4c6e0c46f6' \
+    '6dd10094a02115d1b52c49b39b6cb520077161c9bf636730b1b40e749250743f4524e9e4ba'
+  end
+  let(:transaction_hash) { '0x6c087c1676e8269dd81e0777244584d0cbfd39b6997b3477242a008fa9349e11' }
+  let(:model) do
+    Coinbase::Client::Transaction.new(
+      status: 'pending',
+      unsigned_payload: unsigned_payload
+    )
+  end
+
+  let(:broadcasted_model) do
+    Coinbase::Client::Transaction.new(
+      status: 'broadcast',
+      unsigned_payload: unsigned_payload,
+      signed_payload: signed_payload,
+      transaction_hash: transaction_hash
+    )
+  end
+
+  subject(:transaction) do
+    described_class.new(model, from_address_id: from_address_id)
+  end
+
+  describe '#initialize' do
+    it 'initializes a new Transaction' do
+      expect(transaction).to be_a(Coinbase::Transaction)
+    end
+
+    context 'when initialized with a model of a different type' do
+      it 'raises an error' do
+        expect do
+          described_class.new(Coinbase::Client::Balance.new, from_address_id: from_address_id)
+        end.to raise_error
+      end
+    end
+
+    context 'when initialized without a from_address_id' do
+      it 'raises an error' do
+        expect { described_class.new(model) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#unsigned_payload' do
+    it 'returns the unsigned payload' do
+      expect(transaction.unsigned_payload).to eq(unsigned_payload)
+    end
+  end
+
+  describe '#signed_payload' do
+    context 'when the transaction has not been broadcast on chain' do
+      it 'returns nil' do
+        expect(transaction.signed_payload).to be_nil
+      end
+    end
+
+    context 'when the transaction has been broadcast on chain' do
+      subject(:transaction) do
+        described_class.new(broadcasted_model, from_address_id: from_address_id)
+      end
+
+      it 'returns the signed payload' do
+        expect(transaction.signed_payload).to eq(signed_payload)
+      end
+    end
+  end
+
+  describe '#transaction_hash' do
+    context 'when the transaction has not been broadcast on chain' do
+      it 'returns nil' do
+        expect(transaction.transaction_hash).to be_nil
+      end
+    end
+    context 'when the transaction has been broadcast on chain' do
+      subject(:transaction) do
+        described_class.new(broadcasted_model, from_address_id: from_address_id)
+      end
+
+      it 'returns the transaction hash' do
+        expect(transaction.transaction_hash).to eq(transaction_hash)
+      end
+    end
+  end
+
+  describe '#raw' do
+    it 'returns the rraw transaction' do
+      expect(transaction.raw).to be_a(Eth::Tx::Eip1559)
+    end
+
+    it 'returns the correct amount' do
+      expect(transaction.raw.amount).to eq(amount * Coinbase::WEI_PER_ETHER)
+    end
+
+    it 'returns the correct chain ID' do
+      expect(transaction.raw.chain_id).to eq(Coinbase::BASE_SEPOLIA.chain_id)
+    end
+
+    it 'returns the correct sender address' do
+      expect(transaction.raw.sender).to eq(from_address_id)
+    end
+
+    it 'returns the correct destination address' do
+      expect(transaction.raw.destination).to eq(to_address_id)
+    end
+
+    it 'returns the correct nonce' do
+      expect(transaction.raw.signer_nonce).to eq(0)
+    end
+
+    it 'returns the correct gas limit' do
+      expect(transaction.raw.gas_limit).to eq(21_000)
+    end
+
+    it 'returns the correct max priority fee per gas' do
+      expect(transaction.raw.max_priority_fee_per_gas).to eq(1_500_000_000)
+    end
+  end
+
+  describe '#sign' do
+    subject(:signature) { transaction.sign(from_key) }
+
+    before { signature }
+
+    it 'returns a string' do
+      expect(signature).to be_a(String)
+    end
+
+    it 'signs the raw transaction' do
+      expect(transaction.raw.signature_r).not_to be_empty
+      expect(transaction.raw.signature_s).not_to be_empty
+    end
+
+    it 'returns a hex representation of the signed raw transaction' do
+      expect(signature).to eq(transaction.raw.hex)
+    end
+
+    context 'when it is signed again' do
+      it 'raises an error' do
+        expect { transaction.sign(from_key) }.to raise_error
+      end
+    end
+  end
+
+  describe '#inspect' do
+    it 'includes transaction details' do
+      expect(transaction.inspect).to include(transaction.status.to_s)
+    end
+
+    it 'returns the same value as to_s' do
+      expect(transaction.inspect).to eq(transaction.to_s)
+    end
+
+    context 'when the transaction has been broadcast on chain' do
+      subject(:transaction) do
+        described_class.new(broadcasted_model, from_address_id: from_address_id)
+      end
+
+      it 'includes the transaction hash' do
+        expect(transaction.inspect).to include(transaction.transaction_hash)
+      end
+    end
+  end
+end
