d99a0593b6384ce0ae0384631a3f27f87c15a8af
Jayasudha Jayakumaran <jayasudha.jayakumaran@coinbase.com>
2024-06-14 10:38:29 -0700

    change logic for signer test

--- spec/e2e/end_to_end.rb
+++ spec/e2e/end_to_end.rb
@@ -4,54 +4,61 @@ require 'dotenv'
 Dotenv.load
 
 describe Coinbase do
+  before do
+    # GitHub secrets truncate newlines as whitespace, so we need to replace them.
+    # See https://github.com/github/docs/issues/14207
+    api_key_name = ENV['API_KEY_NAME'].gsub('\n', "\n")
+    api_key_private_key = ENV['API_KEY_PRIVATE_KEY'].gsub('\n', "\n")
+
+    # Use default API URL if not provided
+    api_url = ENV['API_URL']
+
+    Coinbase.configure do |config|
+      config.api_key_name = api_key_name
+      config.api_key_private_key = api_key_private_key
+      config.api_url = api_url if api_url
+    end
+  end
   describe 'v0.0.7 SDK' do
     it 'behaves as expected' do
-      e2e_test
+      user = fetch_user_test
+      new_address = create_new_address_test(user)
+      imported_wallet = import_wallet_test(user)
+      fetch_addresses_balances_test(imported_wallet)
+      imported_address = imported_wallet.addresses[0]
+      transfer_test(imported_address, new_address)
     end
   end
 
-  describe 'use serve signer' do
+  describe 'use for serve signer' do
     it 'behaves as expected' do
       # Use Server-Signer only half the runs to save test time.
-      skip if rand >= 0.5
-      e2e_test(use_server_signer: true)
+      # skip if rand >= 0.5
+      Coinbase.configuration.use_server_signer = true
+      signer = Coinbase::ServerSigner.default
+      puts "Using ServerSigner with ID: #{signer.id}"
+
+      user = fetch_user_test
+      new_address = create_new_address_test(user)
+      existing_wallet = fetch_existing_wallet(user)
+      fetch_addresses_balances_test(existing_wallet)
+      existing_address = existing_wallet.addresses[0]
+      transfer_test(existing_address, new_address)
     end
   end
 end
 
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/AbcSize
-def e2e_test(use_server_signer: false)
-  # GitHub secrets truncate newlines as whitespace, so we need to replace them.
-  # See https://github.com/github/docs/issues/14207
-  api_key_name = ENV['API_KEY_NAME'].gsub('\n', "\n")
-  api_key_private_key = ENV['API_KEY_PRIVATE_KEY'].gsub('\n', "\n")
-
-  # Use default API URL if not provided
-  api_url = ENV['API_URL']
-
-  Coinbase.configure do |config|
-    config.api_key_name = api_key_name
-    config.api_key_private_key = api_key_private_key
-    config.api_url = api_url if api_url
-    config.use_server_signer = use_server_signer
-  end
-
+def fetch_user_test
   puts 'Fetching default user...'
   u = Coinbase.default_user
   expect(u).not_to be_nil
   puts "Fetched default user with ID: #{u.id}"
+  u
+end
 
-  if use_server_signer
-    signer = Coinbase::ServerSigner.default
-    puts "Using ServerSigner with ID: #{signer.id}"
-  end
-
-  data_string = ENV['WALLET_DATA']
-  expect(data_string).not_to be_nil
-
+def create_new_address_test(user)
   puts 'Creating new wallet...'
-  w1 = u.create_wallet
+  w1 = user.create_wallet
   expect(w1).not_to be_nil
   puts "Created new wallet with ID: #{w1.id}, default address: #{w1.default_address}"
 
@@ -60,9 +67,12 @@ def e2e_test(use_server_signer: false)
   expect(new_address).not_to be_nil
   puts "Created new address with ID: #{new_address.id} in wallet with ID #{w1.id}"
 
-  # To move funds from imported wallet, do not use Server-Signer.
-  Coinbase.configuration.use_server_signer = false
+  new_address
+end
 
+def import_wallet_test(user)
+  data_string = ENV['WALLET_DATA']
+  expect(data_string).not_to be_nil
   puts 'Importing wallet with balance...'
 
   data_hash = JSON.parse(data_string)
@@ -72,24 +82,28 @@ def e2e_test(use_server_signer: false)
   expect(data.wallet_id).not_to be_nil
   expect(data.seed).not_to be_nil
 
-  w2 = u.import_wallet(data)
-  expect(w2).not_to be_nil
-  puts "Imported wallet with ID: #{w2.id}, default address: #{w2.default_address}"
+  wallet = user.import_wallet(data)
+  expect(wallet).not_to be_nil
+  puts "Imported wallet with ID: #{wallet.id}, default address: #{wallet.default_address}"
 
+  wallet
+end
+
+def fetch_addresses_balances_test(wallet)
   puts 'Listing wallet addresses...'
-  addresses = w2.addresses
+  addresses = wallet.addresses
   expect(addresses.length).to be > 1
   puts "Listed addresses: #{addresses.map(&:to_s).join(', ')}"
 
   puts 'Fetching wallet balances...'
-  balances = w2.balances
+  balances = wallet.balances
   expect(balances.length).to be >= 1
   puts "Fetched balances: #{balances}"
+end
 
-  puts 'Transfering 1 Gwei from imported address to new address...'
-  imported_address = addresses[0]
-
+def transfer_test(imported_address, new_address)
   # Transfer gwei from imported address to new address.
+  puts 'Transfering 1 Gwei from imported address to new address...'
   t = imported_address.transfer(1, :gwei, new_address).wait!
   expect(t.status).to eq('complete')
   puts "Transferred 1 Gwei from #{imported_address} to #{new_address}"
@@ -98,9 +112,6 @@ def e2e_test(use_server_signer: false)
   t2 = imported_address.transfer(0.00000003, :eth, new_address).wait!
   expect(t2.status).to eq('complete')
   puts "Transferred 0.00000003 Eth from #{imported_address} to #{new_address}"
-
-  # Use Server-Signer if needed for newly created wallet.
-  Coinbase.configuration.use_server_signer = use_server_signer
 
   # Transfer gwei back from new address to imported address.
   t = new_address.transfer(1, :gwei, imported_address).wait!
@@ -116,5 +127,19 @@ def e2e_test(use_server_signer: false)
   puts "New address balances: #{second_balance}"
 end
 
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/AbcSize
+def fetch_existing_wallet(user)
+  # TODO: Change to using get method when available.
+  data_string = ENV['SERVER_SIGNER_WALLET_DATA']
+  expect(data_string).not_to be_nil
+
+  data_hash = JSON.parse(data_string)
+  data = Coinbase::Wallet::Data.from_hash(data_hash)
+  puts "imported wallet id #{data.wallet_id}"
+  expect(data).not_to be_nil
+  expect(data.wallet_id).not_to be_nil
+
+  wallet = user.import_wallet(data)
+  expect(wallet).not_to be_nil
+
+  wallet
+end
