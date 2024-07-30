ebd28b6f3a0c41ed68d321ff2b0a2c34ed7a66be
Dean Galvin <dean.galvin@coinbase.com>
2024-07-30 10:42:17 -0400

    Adds factory bot for transactions

--- spec/factories/keys.rb
+++ spec/factories/keys.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :key, class: Eth::Key do
-    Eth::Key.new(priv: '0233b43978845c03783510106941f42370e0f11022b0c3b717c0791d046f4536')
+    initialize_with { new(priv: priv) }
+
+    priv { '0233b43978845c03783510106941f42370e0f11022b0c3b717c0791d046f4536' }
   end
 end
