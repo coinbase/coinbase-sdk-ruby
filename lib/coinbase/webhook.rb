f57ce942ff33b679f85103a5477045c9e67c88e1
Chaoya Ji <chaoya.ji@coinbase.com>
2024-08-05 10:45:36 -0700

    addressed comments

--- lib/coinbase/webhook.rb
+++ lib/coinbase/webhook.rb
@@ -3,6 +3,8 @@
 module Coinbase
   # A representation of a Webhook.
   class Webhook
+    attr_reader :webhook_id
+
     class << self
       # Creates a new Webhook on the specified Network and generate a default address for it.
       # @param network_id [String]  the ID of the blockchain network.
@@ -19,37 +21,24 @@ module Coinbase
           )
         end
 
+        @webhook_id = model.id
         new(model)
       end
 
-      def list_webhooks_page(page)
+      def list
+        Coinbase::Pagination.enumerate(lambda(&method(:fetch_webhooks_page))) do |webhook|
+          Coinbase::Webhook.new(webhook)
+        end
+      end
+
+      private
+
+      def webhooks_api
+        @webhooks_api ||= Coinbase::Client::WebhooksApi.new(Coinbase.configuration.api_client)
+      end
+
+      def fetch_webhooks_page(page)
         webhooks_api.list_webhooks({ limit: DEFAULT_PAGE_LIMIT, page: page })
-      end
-
-      def update_webhook(webhook_id, network_id:, notification_uri:, event_type:, event_filters:)
-        model = Coinbase.call_api do
-          webhooks_api.update_webhook(
-            webhook_id,
-            update_webhook_request: {
-              network_id: Coinbase.normalize_network(network_id),
-              notification_uri: notification_uri,
-              event_type: event_type,
-              event_filters: event_filters
-            }
-          )
-        end
-
-        new(model)
-      end
-
-      def delete_webhook(webhook_id)
-        model = Coinbase.call_api do
-          webhooks_api.delete_webhook(
-            webhook_id
-          )
-        end
-
-        new(model)
       end
     end
 
@@ -61,8 +50,30 @@ module Coinbase
       @model = model
     end
 
-    def webhooks_api
-      Coinbase::Client::WebhooksApi.new(Coinbase.configuration.api_client)
+    def update_webhook(network_id:, notification_uri:, event_type:, event_filters:)
+      model = Coinbase.call_api do
+        webhooks_api.update_webhook(
+          webhook_id,
+          update_webhook_request: {
+            network_id: Coinbase.normalize_network(network_id),
+            notification_uri: notification_uri,
+            event_type: event_type,
+            event_filters: event_filters
+          }
+        )
+      end
+
+      new(model)
+    end
+
+    def delete_webhook
+      model = Coinbase.call_api do
+        webhooks_api.delete_webhook(
+          webhook_id
+        )
+      end
+
+      new(model)
     end
   end
 end
