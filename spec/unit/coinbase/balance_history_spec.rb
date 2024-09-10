9e3743ecd07d85f44efa20acb7f5f23740a66265
Howard Xie <howard.xie@coinbase.com>
2024-09-09 17:27:48 -0700

    Create balance_history_spec.rb

---
+++ spec/unit/coinbase/balance_history_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Coinbase::Client::BalanceHistoryApi do
+  subject(:address) { described_class.new(network_id) }
+
+  let(:network) { build(:network, :ethereum_mainnet) }
+  let(:network_id) { :ethereum_mainnet }
+  let(:normalized_network_id) { 'ethereum-mainnet' }
+  let(:address_id) { '0x1234' }
+
+  before do
+    allow(Coinbase::Network).to receive(:from_id).with(network_id).and_return(network)
+  end
+
+  describe '#initialize' do
+    it 'initializes a new Address' do
+      expect(address).to be_a(described_class)
+    end
+  end
+
+  describe '#network' do
+    it 'returns the network' do
+      expect(address.network).to eq(network)
+    end
+  end
+
+  describe '#id' do
+    it 'returns the address ID' do
+      expect(address.id).to eq(address_id)
+    end
+  end
+
+  it_behaves_like 'an address that supports balance queries'
+end
+
