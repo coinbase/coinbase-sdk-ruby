4032b8aeedf48cc89618719cbf4ea60ea1b04c07
Howard Xie <howard.xie@coinbase.com>
2024-09-19 17:20:24 -0700

    Update address_transactions.rb

--- spec/support/shared_examples/address_transactions.rb
+++ spec/support/shared_examples/address_transactions.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 shared_examples 'an address that supports transaction queries' do |_operation|
-  let(:external_addresses_api) { instance_double(Coinbase::Client::ExternalAddressesApi) }
   let(:transaction_history_api) { instance_double(Coinbase::Client::TransactionHistoryApi) }
 
   before do
-    allow(Coinbase::Client::ExternalAddressesApi).to receive(:new).and_return(external_addresses_api)
     allow(Coinbase::Client::TransactionHistoryApi).to receive(:new).and_return(transaction_history_api)
   end
 
@@ -37,7 +35,7 @@ shared_examples 'an address that supports transaction queries' do |_operation|
         address.transactions
       end
 
-      let(:api) { external_addresses_api }
+      let(:api) { transaction_history_api }
       let(:fetch_params) { ->(page) { [normalized_network_id, address_id, { limit: 10, page: page }] } }
       let(:resource_list_klass) { Coinbase::Client::AddressTransactionList }
       let(:item_klass) { Coinbase::Transaction }
