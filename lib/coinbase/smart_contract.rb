ff8c922dd6a06deafb1b6bc1d27e513239d1726f
Rohan Agarwal <rohan.agarwal.1@coinbase.com>
2024-10-21 17:30:23 -0700

    Fix

--- lib/coinbase/smart_contract.rb
+++ lib/coinbase/smart_contract.rb
@@ -162,7 +162,8 @@ module Coinbase
       values = solidity_value.values
 
       case type
-      when /^(u?int\d*)$/
+      when 'uint8', 'uint16', 'uint32', 'uint64', 'uint128', 'uint256',
+       'int8', 'int16', 'int32', 'int64', 'int128', 'int256'
         value ? value.to_i : nil
       when 'address', 'string', /^bytes/
         value
